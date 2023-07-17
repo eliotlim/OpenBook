7b78cb69fa4b2da37c067be2ef2e9902d4c7337c
Eliot Lim <eliot.lim@gmail.com>
2023-07-17 22:35:14 +0800

    fix: export dist as files directory
    + still not working
    + but at least it's a different error message!
    + now to fix useContext / hooks
    
    Signed-off-by: Eliot Lim <eliot.lim@gmail.com>

--- packages/hyper-web/src/pages/_app.tsx
+++ packages/hyper-web/src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {CssVarsProvider, CssBaseline} from '@mui/joy'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssVarsProvider defaultMode="system">
+        <CssBaseline/>
+        <Component {...pageProps} />
+      </CssVarsProvider>
+    </>
+  );
 }
