7ed18ca661d5ebf7a1a503cfa3070edf501455db
Eliot Lim <eliot.lim@gmail.com>
2023-09-08 21:01:33 +0800

    feat: emoji picker
    Signed-off-by: Eliot Lim <eliot.lim@gmail.com>

--- packages/hyper-ui/src/screens/PageDocument.tsx
+++ packages/hyper-ui/src/screens/PageDocument.tsx
@@ -1,18 +1,42 @@
 import {Editor} from 'novel';
-import {Stack, Typography} from "@mui/joy";
+import EmojiPicker from 'emoji-picker-react';
+import {Container, Dropdown, Menu, MenuButton, Stack, Typography} from "@mui/joy";
+import React from "react";
 
 const PageDocument = () => {
+  const [emoji, setEmoji] = React.useState("📝");
   return (
     <Stack
-      sx={{
-        paddingTop: 5,
-      }}
       gap={3}
     >
-      <Stack>
-        <Typography level="h1">
-          Untitled Page
-        </Typography>
+      <Stack
+        sx={{
+          paddingTop: 15,
+        }}
+      >
+        <Container>
+          <Stack
+            direction="row"
+            alignItems="center"
+            gap={2}
+          >
+            <Dropdown>
+              <MenuButton>
+                <Typography level="h1">{emoji}</Typography>
+              </MenuButton>
+              <Menu>
+                <EmojiPicker
+                  onEmojiClick={(e) => {
+                    setEmoji(e.emoji);
+                  }}
+                />
+              </Menu>
+            </Dropdown>
+            <Typography level="h1">
+              Untitled Page
+            </Typography>
+          </Stack>
+        </Container>
       </Stack>
       <Editor/>
     </Stack>
