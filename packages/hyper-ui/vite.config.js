1eeed5a5d722c855b61a707b75c8213329538a2d
Eliot Lim <eliot.lim@gmail.com>
2023-09-08 21:53:54 +0800

    chore: exclude emoji-picker-react from compilation
    Signed-off-by: Eliot Lim <eliot.lim@gmail.com>

--- packages/hyper-ui/vite.config.js
+++ packages/hyper-ui/vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig ({
       external: [
         'clsx',
         'csstype',
+        'emoji-picker-react',
         'novel',
         'prop-types',
         'react',
@@ -36,6 +37,7 @@ export default defineConfig ({
         globals: {
           'clsx': 'clsx',
           'csstype': 'csstype',
+          'emoji-picker-react': 'emoji-picker-react',
           'novel': 'novel',
           'prop-types': 'prop-types',
           'react': 'react',
