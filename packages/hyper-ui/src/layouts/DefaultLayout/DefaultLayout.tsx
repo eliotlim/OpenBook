398cc0f5b3884b913917a976b202ce70a8fdbb98
Eliot Lim <eliot.lim@gmail.com>
2023-09-13 20:57:39 +0800

    chore: prepare for sidenav docking
    Signed-off-by: Eliot Lim <eliot.lim@gmail.com>

--- packages/hyper-ui/src/layouts/DefaultLayout/DefaultLayout.tsx
+++ packages/hyper-ui/src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,4 +1,8 @@
 import {NavBar, SideNav} from "@/components";
+import {
+  Box,
+  Stack
+} from "@mui/joy";
 
 export interface DefaultLayoutProps {
   children: React.ReactNode;
@@ -8,8 +12,12 @@ export default function DefaultLayout(props: DefaultLayoutProps) {
   return (
     <>
       <NavBar/>
-      <SideNav/>
-      {props.children}
+      <Stack direction="row" gap={3}>
+        <SideNav/>
+        <Box>
+          {props.children}
+        </Box>
+      </Stack>
     </>
   )
 }
