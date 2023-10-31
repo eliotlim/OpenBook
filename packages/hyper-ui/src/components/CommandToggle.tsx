e2d706e8033562ee7d7ee04f86eeb59f6d9e0272
Eliot Lim <eliot.lim@gmail.com>
2023-10-31 23:02:57 +0800

    feat: add tooltip to search command button
    Signed-off-by: Eliot Lim <eliot.lim@gmail.com>

--- packages/hyper-ui/src/components/CommandToggle.tsx
+++ packages/hyper-ui/src/components/CommandToggle.tsx
@@ -1,23 +1,38 @@
 import {useHud} from "@/providers";
 import {Button} from "@/components/ui/button";
 import {MagnifyingGlassIcon} from "@radix-ui/react-icons";
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
 
 
 export default function CommandToggle() {
   const {setHud} = useHud();
   return (
-    <Button
-      variant="ghost"
-      className="flex flex-grow justify-start h-7"
-      onClick={() => {
-        setHud(draft => {
-          draft.commandPalette.open = !draft.commandPalette.open;
-          return draft;
-        })
-      }}
-    >
-      <MagnifyingGlassIcon className="w-4 h-4 mr-2"/>
-      Search
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="ghost"
+            className="flex flex-grow justify-start h-7"
+            onClick={() => {
+              setHud(draft => {
+                draft.commandPalette.open = !draft.commandPalette.open;
+                return draft;
+              })
+            }}
+          >
+            <MagnifyingGlassIcon className="w-4 h-4 mr-2"/>
+            Search
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent side={"right"} className="flex flex-col bg-background">
+          <span className="text-foreground">
+            Search or perform an action
+          </span>
+          <span className="text-muted-foreground">
+            (Ctrl+K)
+          </span>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
