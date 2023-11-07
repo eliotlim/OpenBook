631a3914e068a3d83a5017614bd4f3d1751a49e9
Eliot Lim <eliot.lim@gmail.com>
2023-11-07 22:52:56 +0800

    feat: rudimentary page tree sidenav
    Signed-off-by: Eliot Lim <eliot.lim@gmail.com>

--- packages/hyper-ui/src/components/WorkspaceNavigationTree.tsx
+++ packages/hyper-ui/src/components/WorkspaceNavigationTree.tsx
@@ -1,81 +1,27 @@
-import {useWorkspace} from "@/providers/WorkspaceProvider";
-import {Tree} from "@/components/Tree";
+import {NavPageRecord, useWorkspace} from "@/providers/WorkspaceProvider";
+import {Tree, TreeDataItem} from "@/components/Tree";
 import {cn} from "@/lib/utils";
 import {useHud} from "@/providers";
+
+export function parsePageTree(pages?: NavPageRecord[]): TreeDataItem[] | undefined {
+  if (!pages) return undefined;
+  return pages.map(page => ({
+    id: page.pageId,
+    name: page.title,
+    children: parsePageTree(page.subPages),
+  }));
+}
 
 export default function WorkspaceNavigationTree() {
   const {hud} = useHud();
   const {workspace} = useWorkspace();
 
-  const data = [
-    {id: "1", name: "Unread"},
-    {id: "2", name: "Threads"},
-    {
-      id: "3",
-      name: "Chat Rooms",
-      children: [
-        {id: "c1", name: "General"},
-        {id: "c2", name: "Random"},
-        {id: "c3", name: "Open Source Projects"},
-      ],
-    },
-    {
-      id: "4",
-      name: "Direct Messages",
-      children: [
-        {
-          id: "d1",
-          name: "Alice",
-          children: [
-            {id: "d11", name: "Alice2"},
-            {id: "d12", name: "Bob2"},
-            {id: "d13", name: "Charlie2"},
-          ],
-        },
-        {id: "d2", name: "Bob"},
-        {id: "d3", name: "Charlie"},
-      ],
-    },
-    {
-      id: "5",
-      name: "Direct Messages",
-      children: [
-        {
-          id: "e1",
-          name: "Alice",
-          children: [
-            {id: "e11", name: "Alice2"},
-            {id: "e12", name: "Bob2"},
-            {id: "e13", name: "Charlie2"},
-          ],
-        },
-        {id: "e2", name: "Bob"},
-        {id: "e3", name: "Charlie"},
-      ],
-    },
-    {
-      id: "6",
-      name: "Direct Messages",
-      children: [
-        {
-          id: "f1",
-          name: "Alice",
-          children: [
-            {id: "f11", name: "Alice2"},
-            {id: "f12", name: "Bob2"},
-            {id: "f13", name: "Charlie2"},
-          ],
-        },
-        {id: "f2", name: "Bob"},
-        {id: "f3", name: "Charlie"},
-      ],
-    },
-  ];
+  const data = parsePageTree(workspace?.pages);
 
   return (
     <>
       <Tree
-        data={data}
+        data={data ?? []}
         className={cn("w-full border-0", hud.sideNav.docked ? `h-[calc(100vh-12rem)]` : `h-[calc(100vh-20rem)]`)}
         initialSlelectedItemId="f12"
         // onSelectChange={(item) => setContent(item?.name ?? "")}
