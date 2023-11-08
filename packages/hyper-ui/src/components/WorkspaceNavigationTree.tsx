a66d30d131ad9571fee10cd0f103d46f3a34a47b
Eliot Lim <eliot.lim@gmail.com>
2023-11-08 23:59:14 +0800

    feat: lucide icon navtree
    Signed-off-by: Eliot Lim <eliot.lim@gmail.com>

--- packages/hyper-ui/src/components/WorkspaceNavigationTree.tsx
+++ packages/hyper-ui/src/components/WorkspaceNavigationTree.tsx
@@ -2,12 +2,14 @@ import {NavPageRecord, useWorkspace} from "@/providers/WorkspaceProvider";
 import {Tree, TreeDataItem} from "@/components/Tree";
 import {cn} from "@/lib/utils";
 import {useHud} from "@/providers";
+import {FolderIcon} from 'lucide-react';
 
 export function parsePageTree(pages?: NavPageRecord[]): TreeDataItem[] | undefined {
   if (!pages) return undefined;
   return pages.map(page => ({
     id: page.pageId,
     name: page.title,
+    icon: FolderIcon,
     children: parsePageTree(page.subPages),
   }));
 }
