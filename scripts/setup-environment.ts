77eb80fd082177d4e641199534962c5ee2375261
Eliot Lim <eliot.lim@gmail.com>
2024-09-21 22:28:17 +0800

    fix: improve setup script
    Signed-off-by: Eliot Lim <eliot.lim@gmail.com>

--- scripts/setup-environment.ts
+++ scripts/setup-environment.ts
@@ -2,12 +2,17 @@ import * as child_process from "node:child_process";
 
 function setupEnvironment() {
   console.log('Linking subpackages...');
-  [
-    'packages/ui',
-  ].map((packagePath) => {
+
+  const subpackages: Record<string, string> = {
+    "@open-book/ui": "packages/ui",
+  };
+
+  Object.keys(subpackages).map((packageName) => {
+    const packagePath = subpackages[packageName];
     try {
-      console.log(`Linking ${packagePath}`)
+      console.log(`Linking ${packageName} to ${packagePath}`)
       child_process.execSync(`cd ${packagePath} && npm link`);
+      child_process.execSync(`npm link ${packageName}`)
     } catch (e) {
       console.error(e);
     }
